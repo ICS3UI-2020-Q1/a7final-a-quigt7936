ae7c181ad1022ffa4a2498e8357ae8bf7ae96b8b
Thomas Quigley <quigt7936@wrdsb.ca>
2020-10-14 13:40:47 +0000

    Completed question

--- Main.java
+++ Main.java
@@ -1,15 +1,51 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * The program tells the user how many 1 digits it has
+ * @author Thomas Quigley
  */
 public class Main {
+  // tells how many 1's are in a number
+  public static void oneDigits(int userNumber) {
+    // create the variable that counts the ones 
+    int countOnes = 0;
 
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
+    // a while loop that stops when all the digits of the number have been checked
+    while (userNumber > 0) {
+     // create a variable that is the users number but without the last digit
+      int oneOrNot = userNumber;
+
+      // divide oneOrNot by 10 then multiple so the last digit is 0
+      oneOrNot = oneOrNot / 10;
+      oneOrNot = oneOrNot * 10;
+
+      // create the last digit variable
+      int lastDigit;
+
+      // if oneOrNot is 0 then the userNumber is the last digit
+      if (oneOrNot == 0) {
+        lastDigit = userNumber;
+      } else {
+        // get the value of the last digit
+        lastDigit = userNumber % oneOrNot;
+      }
+      // check if the last digit is 1
+      if (lastDigit == 1) {
+        countOnes = countOnes + 1;
+      }
+      // divide by 10 and go onto the next number
+      userNumber = userNumber / 10;
+    }
+    // tell the user how many ones there are in their number
+    System.out.println("There are " + countOnes + " ones in your number.");
+  }
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    // ask the user for their number
+    System.out.println("What number would you like to test?");
+    int userNumber = input.nextInt();
+
+    // peform method oneDigits
+    oneDigits(userNumber);
     
   }
 }
